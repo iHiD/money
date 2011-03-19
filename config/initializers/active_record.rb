c6e6d19213cdf5986612e0fa95dd2f70f14e13c8
Jeremy Walker <jez.walker@gmail.com>
2011-03-19 15:53:40 +0000

    Change t.timestamps to be NOT NULL by default

---
+++ config/initializers/active_record.rb
@@ -0,0 +1,12 @@
+module ActiveRecord
+  module ConnectionAdapters # :nodoc:
+    class TableDefinition
+      def timestamps(*args)
+        options = args.extract_options!
+        options[:null] = false unless options[:null]
+        column(:created_at, :datetime, options)
+        column(:updated_at, :datetime, options)
+      end
+    end
+  end
+end
