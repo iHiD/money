d8786e3f62413ff2e9e8aa2967b2bae05fbacd95
Jeremy Walker <jez.walker@gmail.com>
2011-03-19 15:48:24 +0000

    Initial Project Setup

---
+++ Gemfile
@@ -0,0 +1,12 @@
+source 'http://rubygems.org'
+
+gem 'rails', '3.0.5'
+gem 'pg'
+
+# gem 'ruby-debug19', :require => 'ruby-debug'
+
+group :development, :test do
+	gem 'rspec-rails'
+	gem 'cucumber-rails'
+    gem 'webrat'
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Money::Application.config.secret_token = 'acc07ad4bef23eaa4ab6d00da1adbf429dc1c079bc2f9a2876996b9af5b9b0d425f272d5308a07321fa8b84f47f2fddf293537d564743dd7793d5a77ab6dfa29'
